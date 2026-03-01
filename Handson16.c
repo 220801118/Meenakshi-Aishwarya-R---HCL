08fc17673b6ee513acd8f2ef1a2b6f50b87bbced
220801118 <220801118@rajalakshmi.edu.in>
2026-03-01 18:09:43 +0000

    Sample

--- Handson16.c
+++ Handson16.c
@@ -1,35 +1,40 @@
 #include <stdio.h>
-int findSubstring(const char *text, const char *pattern)
+
+int findSubstring(char text[], char pattern[])
 {
-    int i,j;
-    for (i=0;text[i]!='\0';i++)
+    int i, j;
+
+    for (i = 0; text[i] != '\0'; i++)
     {
-        j=0;
-        while (pattern[j]!='\0' && text[i + j]!='\0' &&
-               text[i + j]==pattern[j])
+        for (j = 0; pattern[j] != '\0'; j++)
         {
-            j++;
+            if (text[i + j] != pattern[j])
+                break;
         }
-        if (pattern[j]=='\0')
-        {
-            return i;  
-        }
+
+        if (pattern[j] == '\0')   // full pattern matched
+            return i;
     }
-    return -1; 
+
+    return -1;
 }
+
 int main()
 {
     char text[200], pattern[100];
-    fgets(text, sizeof(text), stdin);
-    fgets(pattern, sizeof(pattern), stdin);
-    for (int i=0; text[i]!='\0';i++)
-        if (text[i]=='\n') { text[i]='\0'; break; }
-    for (int i=0; pattern[i]!='\0'; i++)
-        if (pattern[i]=='\n') { pattern[i]='\0'; break; }
-    int index=findSubstring(text, pattern);
-    if (index!=-1)
+
+    printf("Enter text: ");
+    scanf("%s", text);
+
+    printf("Enter pattern: ");
+    scanf("%s", pattern);
+
+    int index = findSubstring(text, pattern);
+
+    if (index != -1)
         printf("Substring found at index %d\n", index);
     else
         printf("Substring not found\n");
+
     return 0;
 }
